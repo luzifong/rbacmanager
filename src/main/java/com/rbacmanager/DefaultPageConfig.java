dc72e2307739e287f5ca9e2e6891fe5fab055398
luzifong <luzifengms@outlook.com>
2019-09-03 13:51:04 +0800

    update

--- src/main/java/com/rbacmanager/DefaultPageConfig.java
+++ src/main/java/com/rbacmanager/DefaultPageConfig.java
@@ -5,13 +5,13 @@ import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-public class DefaultPageConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("login");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        super.addViewControllers(registry);
-    }
-}
+//@Configuration
+//public class DefaultPageConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("login");
+//        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        super.addViewControllers(registry);
+//    }
+//}
--- src/main/java/com/rbacmanager/RbacmanagerApplication.java
+++ src/main/java/com/rbacmanager/RbacmanagerApplication.java
@@ -3,6 +3,8 @@ package com.rbacmanager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @MapperScan("com.rbacmanager.mapper")
@@ -11,5 +13,11 @@ public class RbacmanagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RbacmanagerApplication.class, args);
     }
-
+    @Bean
+    public InternalResourceViewResolver setUpViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
